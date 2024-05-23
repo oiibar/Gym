37c77d77905ac614feb0e8d84f0bab7468e99159
oiibar <95119839+oiibar@users.noreply.github.com>
2024-05-23 19:47:39 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://gym-server-seven.vercel.app/"],
+    origin: [""],
     methods: ["POST", "GET", "DELETE"],
     credentials: true,
   })
