1617d72d9800f53eb1987dde86d4cec55477509e
oiibar <95119839+oiibar@users.noreply.github.com>
2024-05-28 14:52:55 +0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("https://gym-backend-esdn.onrender.com/api/workouts", {
+      const response = await fetch("https://gym-serv.onrender.com/api/workouts", {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       });
