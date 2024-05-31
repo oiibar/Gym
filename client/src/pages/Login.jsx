a13b6514e515d86d634d91321e7d2e07519cb9e0
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-05-31 19:03:02 +0500

    auth navigation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
-import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login, error, isLoading } = useLogin();
-  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const success = await login(email, password);
     if (success) {
-      navigate("/");
+      console.log("navigated");
     }
   };
 
